a6965c62f4bd8752f26299537f1f18d529cd094a
Misha-Romanovsky <romanovskyy.mykhaylo@chnu.edu.ua>
2022-04-17 13:17:37 +0300

    Home section ready

--- js/script.js
+++ js/script.js
@@ -8,3 +8,12 @@ function stickyNavbar(){
 stickyNavbar();
 
 window.addEventListener("scroll", stickyNavbar);
+
+// Reveal Anim
+
+let sr = ScrollReveal({
+    duration: 2500,
+    distance: "60px",
+});
+sr.reveal(".showcase-info", {delay: 600});
+sr.reveal(".showcase-image", {origin: "top", delay: 700});
